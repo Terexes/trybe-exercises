17e6704698b8d5b1ba36444fa4522799bfacfd4c
Rafael Moreci <rafamoreci@gmail.com>
2021-11-29 18:46:36 -0300

    Exercícios dia 1

--- 01-Fundamentos/bloco-04-introducao-ao-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/script.js
+++ 01-Fundamentos/bloco-04-introducao-ao-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/script.js
@@ -34,7 +34,23 @@ Seu programa também deve emitir uma mensagem de erro e encerrar caso algum dos 
 O lucro de um produto é o resultado da subtração do valor de venda pelo custo do mesmo, sendo que o imposto de 20% também faz parte do valor de custo.
 valorCustoTotal = valorCusto + impostoSobreOCusto
 lucro = valorVenda - valorCustoTotal (lucro de um produto)
+
+11-) Uma pessoa que trabalha de carteira assinada no Brasil tem descontados de seu salário bruto o INSS e o IR. Faça um programa que, dado um salário bruto, calcule o líquido a ser recebido.
+A notação para um salário de R$1500,10, por exemplo, deve ser 1500.10. Para as faixas de impostos, use as seguintes referências:
+INSS (Instituto Nacional do Seguro Social)
+Salário bruto até R$ 1.556,94: alíquota de 8%
+Salário bruto de R$ 1.556,95 a R$ 2.594,92: alíquota de 9%
+Salário bruto de R$ 2.594,93 a R$ 5.189,82: alíquota de 11%
+Salário bruto acima de R$ 5.189,82: alíquota máxima de R$ 570,88
+IR (Imposto de Renda)
+Até R$ 1.903,98: isento de imposto de renda
+De R$ 1.903,99 a 2.826,65: alíquota de 7,5% e parcela de R$ 142,80 a deduzir do imposto
+De R$ 2.826,66 a R$ 3.751,05: alíquota de 15% e parcela de R$ 354,80 a deduzir do imposto
+De R$ 3.751,06 a R$ 4.664,68: alíquota de 22,5% e parcela de R$ 636,13 a deduzir do imposto
+Acima de R$ 4.664,68: alíquota de 27,5% e parcela de R$ 869,36 a deduzir do imposto.
+
 */
+
 //1
 function operadores(operador, a, b) {
   switch (operador) {
@@ -162,10 +178,42 @@ function calcLucro(custo, valorDeVenda) {
   // Cálculo para 1000 un.
   valorCustoTotal *= 1000;
   lucro *= 1000;
-  console.log(valorCustoTotal, lucro);
   if (lucro < valorCustoTotal) {
     return 'Prejuízo';
   } else {
     return 'Lucro';
   }
 }
+
+function calcSalarioLiquido(salarioBruto) {
+  let aliquotaInss = 0;
+  let aliquotaIR = 0;
+  let salarioBase = 0;
+  //INSS:
+  if ((salarioBruto > 5189, 82)) {
+    aliquotaInss = 570.88;
+  } else if (salarioBruto <= 5189.82 && salarioBruto >= 2594.93) {
+    aliquotaInss = salarioBruto * 0.11;
+  } else if (salarioBruto <= 2594.92 && salarioBruto >= 1556.95) {
+    aliquotaInss = salarioBruto * 0.09;
+  } else {
+    aliquotaInss = salarioBruto * 0.08;
+  }
+  salarioBase = salarioBruto - aliquotaInss;
+  //IR
+  if (salarioBase > 4664.68) {
+    aliquotaIR = salarioBase * 0.275 - 869.36;
+  } else if (salarioBase <= 4664.68 && salarioBase >= 3751.06) {
+    aliquotaIR = salarioBase * 0.225 - 636.13;
+  } else if (salarioBase <= 3751.05 && salarioBase >= 2826.66) {
+    aliquotaIR = salarioBase * 0.15 - 354.8;
+  } else if (salarioBase <= 2826.65 && salarioBase >= 1903.99) {
+    aliquotaIR = salarioBase * 0.075 - 142.8;
+  } else {
+    aliquotaIR = 0;
+  }
+
+  return (salarioBase - aliquotaIR).toFixed(2);
+}
+
+console.log(calcSalarioLiquido(3200));
