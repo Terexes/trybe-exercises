a00e84d8f64c59b351bb0cd87ccadcdea131023d
Rafael Moreci <rafamoreci@gmail.com>
2021-11-29 15:44:40 -0300

    Resoluções

--- 01-Fundamentos/bloco-04-introducao-ao-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/script.js
+++ 01-Fundamentos/bloco-04-introducao-ao-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/script.js
@@ -14,6 +14,19 @@ Módulo (a % b)
 
 5-)Faça um programa que defina três constantes com os valores dos três ângulos internos de um triângulo. Retorne true se os ângulos representarem os ângulos de um triângulo e false , caso contrário. Se algum ângulo for inválido o programa deve retornar uma mensagem de erro.
 
+6-) Escreva um programa que receba o nome de uma peça de xadrez e retorne os movimentos que ela faz.
+
+7 -) Escreva um programa que converte uma nota dada em porcentagem (de 0 a 100) em conceitos de A a F. Siga essas regras:
+Porcentagem >= 90 -> A
+Porcentagem >= 80 -> B
+Porcentagem >= 70 -> C
+Porcentagem >= 60 -> D
+Porcentagem >= 50 -> E
+Porcentagem < 50 -> F
+
+8-) Escreva um programa que defina três números em constantes e retorne true se pelo menos uma das três for par. Caso contrário, ele retorna false .
+
+9-) Escreva um programa que defina três números em constantes e retorne true se pelo menos uma das três for ímpar. Caso contrário, ele retorna false .
 */
 //1
 function operadores(operador, a, b) {
@@ -65,8 +78,69 @@ function checaValor(a) {
 }
 
 //5
-function checaTriangulo(a,b,c) {
+function checaTriangulo(a, b, c) {
   if (a < 0 || b < 0 || c < 0) {
-    return `O angulo ${}`
+    return `Valor inválido informado`;
+  }
+  return a + b + c == 180 ? true : false;
+}
+
+// 6
+
+function movXadrez(peca) {
+  switch (peca) {
+    case 'rei':
+      return 'O rei  pode mover-se em todas as direções (horizontal, vertical e diagonal) somente uma casa de cada vez.';
+    case 'rainha':
+      return 'A rainha move-se ao longo da horizontal, vertical e diagonais mas não pode pular outras peças.';
+    case 'bispo':
+      return 'O bispo move-se ao longo da diagonal. Não pode pular outras peças.';
+    case 'cavalo':
+      return 'É a única peça que pode pular as outras. O movimento do cavalo é em forma de “L”, quer dizer, duas casas em sentido horizontal e mais uma na vertical ou vice-versa.';
+    case 'torre':
+      return 'A torre movimenta-se pela vertical ou horizontal, mas não pode pular outras peças.';
+    case 'peao':
+      return 'O peão movimenta-se apenas uma casa para frente e somente captura outras peças na diagonal. Opcionalmente, cada peão pode avançar duas casas no seu primeiro movimento do jogo.';
+    default:
+      return 'Informe uma peça válida.';
+      break;
+  }
+}
+
+// 7
+function checaNota(nota) {
+  if (nota > 100 || nota < 0) {
+    return 'Informe um valor válido.';
+  }
+  if (nota >= 90) {
+    return 'A';
+  } else if (nota >= 80) {
+    return 'B';
+  } else if (nota >= 70) {
+    return 'C';
+  } else if (nota >= 60) {
+    return 'D';
+  } else if (nota >= 50) {
+    return 'E';
+  } else if (nota < 50) {
+    return 'F';
+  }
+}
+
+// 8
+function checaPar(a, b, c) {
+  if (a % 2 === 0 || b % 2 === 0 || c % 2 === 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// 9
+function checaImpar(a, b, c) {
+  if (a % 2 !== 0 || b % 2 !== 0 || c % 2 !== 0) {
+    return true;
+  } else {
+    return false;
   }
 }
